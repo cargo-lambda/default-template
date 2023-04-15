2249c84f6f1ba89e762318c36c940d4538f18716
David Calavera <david.calavera@gmail.com>
2023-04-15 10:17:37 -0700

    Use http request event (#6)
    This is a better starter than not doing anything
    
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -51,15 +51,20 @@ async fn function_handler(event: LambdaEvent<Request>) -> Result<Response, Error
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(_event: Request) -> Result<Response<Body>, Error> {
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
+    let who = event
+        .query_string_parameters_ref()
+        .and_then(|params| params.first("name"))
+        .unwrap_or("world");
+    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body("Hello AWS Lambda HTTP request".into())
+        .body(message.into())
         .map_err(Box::new)?;
     Ok(resp)
 }
